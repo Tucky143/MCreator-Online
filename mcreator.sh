a6da17063965f5f4dd2c6f7770e3e2b962225e86
Tucky143 <110123789+Tucky143@users.noreply.github.com>
2025-07-26 15:14:10 -0600

    Update mcreator.sh

--- mcreator.sh
+++ mcreator.sh
@@ -1,3 +1,41 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f2d919e6098e821762bbdeac198c08c602203997e7d53d1b698d498f75d6eece
-size 1001
+#!/bin/bash
+#
+# ---------------------------------------------------------------------
+# MCreator Launcher for Unix Like systems with Launcher creation
+# ---------------------------------------------------------------------
+#
+
+# make launcher desktop file
+if [[ ! -f "mcreator.desktop" ]]; then
+
+pwd=$(pwd)
+
+cat > mcreator.desktop <<EOL
+[Desktop Entry]
+Exec=/bin/bash -c 'cd "${pwd}" && ./mcreator.sh'
+Type=Application
+Terminal=false
+Name=MCreator
+Icon=${pwd}/icon.png
+EOL
+
+chmod +x mcreator.desktop
+
+# Is xdg-user-dir available?
+if ! command -v xdg-user-dir &> /dev/null
+then
+    # Use dynamic desktop directory for other languages (#3123)
+    userdesktop=$(xdg-user-dir DESKTOP)
+else
+    # Fall back to hardcoded ~/Desktop
+    userdesktop=~/Desktop
+fi
+cp mcreator.desktop "${userdesktop}"/mcreator.desktop
+
+fi
+
+# set the classpath
+export CLASSPATH='./lib/mcreator.jar:./lib/*'
+
+# launch MCreator with bundled java
+./jdk/bin/java --add-opens=java.base/java.lang=ALL-UNNAMED net.mcreator.Launcher "$1"
